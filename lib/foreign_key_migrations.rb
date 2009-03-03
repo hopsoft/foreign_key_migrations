20169037f76c25789d69b24cbfd9a67503ace0d1
Nathan Hopkins <natehop@gmail.com>
2009-03-03 13:42:38 -0700

    Fixed a bug with remove_foreign_key.

--- lib/foreign_key_migrations.rb
+++ lib/foreign_key_migrations.rb
@@ -8,18 +8,18 @@ module ActiveRecord
       # ===Params
       # * table_name - the name of the database table being altered
       # * foreign_key - name of foreign key column or an array of fk column names
-      # * references - name of the table that the foreign key references
+      # * references - name of primary key column or an array of pk column names
       # * options - hash of additional options accepted options: * :name - name of
       #   constraint * :on_update - referential action to take (CASCADE, RESTRICT, NO
       #   ACTION, SET NULL, SET DEFAULT) * :on_delete - referential action to take
       #   (CASCADE, RESTRICT, NO ACTION, SET NULL, SET DEFAULT)
       def add_foreign_key(table_name, foreign_key, references, options={})
-        # prep args
-        table_name = table_name.to_s
-        foreign_key = foreign_key.to_s if foreign_key.is_a?(Symbol)
-        references = references.to_s if references.is_a?(Symbol)
-        foreign_key.collect! {|x| x.to_s } if foreign_key.is_a?(Array)
-        references.collect! {|x| x.to_s } if references.is_a?(Array)
+        prep_args(table_name, foreign_key, references, options) do |t, f, r, o|
+          table_name = t
+          foreign_key = f
+          references = r
+          options = o
+        end
 
         query = "ALTER TABLE [#{table_name}] "
         constraint_name = get_constraint_name(table_name, foreign_key, references, options)
@@ -44,35 +44,50 @@ module ActiveRecord
 
       # Attempts to remove a foreign key using ANSI SQL Standard syntax,
       # so most database providers should work just fine.
-      # 
+      #
       # ===Params
       # * table_name - the name of the database table being altered
       # * foreign_key - name of foreign key column or an array of fk column names
-      # * references - name of the table that the foreign key references
+      # * references - name of primary key column or an array of pk column names
       # * options - hash of additional options accepted options: * :name - name of
       #   constraint * :on_update - referential action to take (CASCADE, RESTRICT, NO
       #   ACTION, SET NULL, SET DEFAULT) * :on_delete - referential action to take
       #   (CASCADE, RESTRICT, NO ACTION, SET NULL, SET DEFAULT)
       def remove_foreign_key(table_name, foreign_key, references, options={})
+        prep_args(table_name, foreign_key, references, options) do |t, f, r, o|
+          table_name = t
+          foreign_key = f
+          references = r
+          options = o
+        end
+
         constraint_name = get_constraint_name(table_name, foreign_key, references, options)
-        query = "ALTER TABLE DROP CONSTRAINT #{constraint_name}"
+        query = "ALTER TABLE [#{table_name}] DROP CONSTRAINT [#{constraint_name}]"
         begin
-          # try it the ANSI SQL way first
           execute(query)
         rescue
-          # try it the MySQL way
-          query = "ALTER TABLE DROP FOREIGN KEY #{constraint_name}"
+          query = "ALTER TABLE [#{table_name}] DROP FOREIGN KEY [#{constraint_name}]"
           execute(query)
         end
       end
 
       private
 
+      # Prepares the arguments for use and yields them back.
+      def prep_args(table_name, foreign_key, references, options={})
+        table_name = table_name.to_s
+        foreign_key = foreign_key.to_s if foreign_key.is_a?(Symbol)
+        references = references.to_s if references.is_a?(Symbol)
+        foreign_key.collect! {|x| x.to_s } if foreign_key.is_a?(Array)
+        references.collect! {|x| x.to_s } if references.is_a?(Array)
+        yield(table_name, foreign_key, references, options)
+      end
+
       # Gets the constraint name based on the options.
       def get_constraint_name(table_name, foreign_key, references, options={})
         constraint_name = options[:name]
         unless constraint_name
-          constraint_name = table_name
+          constraint_name = String.new(table_name) # create a copy of table_name so its not modified
           constraint_name << "_" + foreign_key if foreign_key.is_a?(String)
           constraint_name << "_" + foreign_key.join("_") if foreign_key.is_a?(Array)
           constraint_name << "_" + references if references.is_a?(String)
